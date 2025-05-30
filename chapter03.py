849fd8ac2373664e139b47bcbad326afdcadb43a
Arjun.Krishna <arjunkrishna1282@gmail.com>
2025-05-30 23:26:08 +0530

    result

--- chapter03.py
+++ chapter03.py
@@ -253,5 +253,32 @@ class MultiHeadAttention(nn.Module):
         context_vec = context_vec.contiguous().view(
             b, num_tokens, self.d_out
         )
-        context_vec = self.out_proj(context_vec)
+        context_vec = self.out_proj(context_vec) # adds an optional linear projection
         return context_vec
+
+# to illustrate batches matrix multiplicaton eg:
+a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573],
+                    [0.8993, 0.0390, 0.9268, 0.7388],
+                    [0.7179, 0.7058, 0.9156, 0.4340]],
+
+                    [[0.0772, 0.3565, 0.1479, 0.5331],
+                    [0.4066, 0.2318, 0.4545, 0.9737],
+                    [0.4606, 0.5159, 0.4220, 0.5786]]]])
+
+first_head = a[0, 0, :, :]
+first_res = first_head @ first_head.T
+print("First head:\n", first_res)
+
+second_head = a[0, 1, :, :]
+second_res = second_head @ second_head.T
+print("\nSecond head:\n", second_res)
+
+print('same as above : ',a @ a.transpose(2, 3)) # will obtain same as the above batched mat_mul 
+
+torch.manual_seed(123)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
