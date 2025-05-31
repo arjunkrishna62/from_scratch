d461c0a4594e7fa9eb38a76a4130fc1a5769907b
Arjun.Krishna <arjunkrishna1282@gmail.com>
2025-05-31 18:15:56 +0530

    into finetunig

--- chapter03.py
+++ chapter03.py
@@ -257,23 +257,23 @@ class MultiHeadAttention(nn.Module):
         return context_vec
 
 # to illustrate batches matrix multiplicaton eg:
-a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573],
-                    [0.8993, 0.0390, 0.9268, 0.7388],
-                    [0.7179, 0.7058, 0.9156, 0.4340]],
+# a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573],
+#                     [0.8993, 0.0390, 0.9268, 0.7388],
+#                     [0.7179, 0.7058, 0.9156, 0.4340]],
 
-                    [[0.0772, 0.3565, 0.1479, 0.5331],
-                    [0.4066, 0.2318, 0.4545, 0.9737],
-                    [0.4606, 0.5159, 0.4220, 0.5786]]]])
+#                     [[0.0772, 0.3565, 0.1479, 0.5331],
+#                     [0.4066, 0.2318, 0.4545, 0.9737],
+#                     [0.4606, 0.5159, 0.4220, 0.5786]]]])
 
-first_head = a[0, 0, :, :]
-first_res = first_head @ first_head.T
-print("First head:\n", first_res)
+# first_head = a[0, 0, :, :]
+# first_res = first_head @ first_head.T
+# print("First head:\n", first_res)
 
-second_head = a[0, 1, :, :]
-second_res = second_head @ second_head.T
-print("\nSecond head:\n", second_res)
+# second_head = a[0, 1, :, :]
+# second_res = second_head @ second_head.T
+# print("\nSecond head:\n", second_res)
 
-print('same as above : ',a @ a.transpose(2, 3)) # will obtain same as the above batched mat_mul 
+# print('same as above : ',a @ a.transpose(2, 3)) # will obtain same as the above batched mat_mul 
 
 torch.manual_seed(123)
 batch_size, context_length, d_in = batch.shape
@@ -282,3 +282,4 @@ mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 context_vecs = mha(batch)
 print(context_vecs)
 print("context_vecs.shape:", context_vecs.shape)
+
