1920a81d57d92369c2a5909c5ce5a4a4658045d6
Arjun.Krishna <arjunkrishna1282@gmail.com>
2025-06-01 18:18:06 +0530

    layer norm

--- chapter05.py
+++ chapter05.py
@@ -23,14 +23,15 @@ class DummyGPTModel(nn.Module):
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
-        self.trf_blocks = nn.Sequential(
+        self.trf_blocks = nn.Sequential( # placeholder for tfr blocks
             *[DummyTransformerBlock(cfg)
             for _ in range(cfg["n_layers"])]
         )
-        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"]) # layer norm placeholder 
         self.out_head = nn.Linear(
-        cfg["emb_dim"], cfg["vocab_size"], bias=False
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
         )
+
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx)
@@ -55,3 +56,68 @@ class DummyLayerNorm(nn.Module):
         super().__init__()
     def forward(self, x):
         return x
+    
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print('from 124m :- ',logits)
+
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+# before layer norm compute mean and var
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# applying layer noramlization to the output
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# note that the value –5.9605e-08 in the output tensor is the scientific notation for
+# –5.9605 × 10-8, which is –0.000000059605 in decimal form. This value is very close to 0,
+# but it is not exactly 0 due to small numerical errors that can accumulate because of
+# the finite precision with which computers represent numbers.
+
+# after removiing the scientific notation
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# Layer noramalization classs
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+        
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
